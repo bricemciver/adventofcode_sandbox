0c0f04af9a0cd39c0b053e9c372f60197d623ec1
Brice McIver <github@bricemciver.com>
2023-12-06 09:38:03 -0600

    feat: completed day 3

--- 2023/day3.py
+++ 2023/day3.py
@@ -26,7 +26,30 @@
 
 # Of course, the actual engine schematic is much larger. What is the sum of all of the part numbers in the engine schematic?
 
-# 517866 is too low
+# 517866 is too low (part 1)
+# 22920611 is too low (part 2)
+
+def get_number_from_matrix(matrix: list[list[str]], x: int, y: int, direction: str):
+    num = []
+    if direction == "left":
+        starting_x = x
+        while starting_x >= 0 and matrix[y][starting_x].isdigit():
+            starting_x = starting_x-1
+        starting_x = starting_x+1
+        while matrix[y][starting_x].isdigit():
+            num.append(matrix[y][starting_x])
+            starting_x = starting_x + 1
+        return int(''.join(num))
+    if direction == "right":
+        ending_x = x
+        while ending_x < len(matrix[y]) and matrix[y][ending_x].isdigit():
+            ending_x = ending_x+1
+        ending_x = ending_x-1
+        while matrix[y][ending_x].isdigit():
+            num.append(matrix[y][ending_x])
+            ending_x = ending_x - 1
+        num.reverse()
+        return int(''.join(num))
 
 # Open the file and store the file handle in f
 f = open("2023/day3.input.txt", "r")
@@ -37,34 +60,86 @@ for line in f:
 
 total = 0
 for i in range(len(matrix)):
+    # find gears
     start_digit = -1
     end_digit = -1
     for j in range(len(matrix[i])):
-        if str(matrix[i][j]).isdigit():
-            if start_digit == -1:
-                start_digit = j
-            elif j == len(matrix[i])-1 and end_digit == -1:
-                end_digit = j
-        else:
-            if end_digit == -1 and start_digit != -1:
-                end_digit = j - 1
-        if start_digit >= 0 and end_digit >= 0:
-            # now that we have a number, check if a symbol exists
-            for k in range(max(0, i - 1), min(len(matrix), i + 2)):
-                for m in range(
-                    max(0, start_digit - 1), min(len(matrix[i]), end_digit + 2)
-                ):
-                    part_num_array = []
-                    if not matrix[k][m].isdigit() and matrix[k][m] != ".":
-                        # this one counts, find the number
-                        idx = start_digit
-                        while idx < end_digit + 1:
-                            part_num_array.append(matrix[i][idx])
-                            idx = idx + 1
-                        part_num = int("".join(part_num_array))
-                        print(part_num)
-                        total = total + part_num
-            start_digit = -1
-            end_digit = -1
+        if str(matrix[i][j]) == '*':#.isdigit():
+            nums_found = 0
+            nums = []
+            # we found a gear, check if at least two sides have a number
+            if matrix[i][max(0,j-1)].isdigit():
+                nums_found = nums_found + 1
+                nums.append(get_number_from_matrix(matrix, j-1, i, direction="left"))
+            if matrix[i][min(len(matrix[i]),j+1)].isdigit():
+                nums_found = nums_found + 1
+                nums.append(get_number_from_matrix(matrix, j+1, i, direction="right"))
+            # check 3 top and 3 bottom positions
+            largest_x = len(matrix[i])
+            largest_y = len(matrix)
+            top_left = matrix[max(0,i-1)][max(0, j-1)]
+            top_center = matrix[max(0,i-1)][j]
+            top_right = matrix[max(0,i-1)][min(j+1, largest_x)]
+            bottom_left = matrix[min(largest_y,i+1)][max(0, j-1)]
+            bottom_center = matrix[min(largest_y,i+1)][j]
+            bottom_right = matrix[min(largest_y,i+1)][min(j+1, largest_x)]
+            if top_left.isdigit():
+                if top_center.isdigit():
+                    nums_found = nums_found + 1
+                    nums.append(get_number_from_matrix(matrix, j, i-1, direction="left"))
+                elif top_right.isdigit():
+                    nums_found = nums_found + 2
+                    nums.append(get_number_from_matrix(matrix, j-1, i-1, direction="left"))
+                    nums.append(get_number_from_matrix(matrix, j+1, i-1, direction="right"))
+                else:
+                    nums_found = nums_found + 1
+                    nums.append(get_number_from_matrix(matrix, j-1, i-1, direction="left"))
+            elif top_right.isdigit():
+                nums_found = nums_found + 1
+                nums.append(get_number_from_matrix(matrix, j+1, i-1, direction="right"))
+            if bottom_left.isdigit():
+                if bottom_center.isdigit():
+                    nums_found = nums_found + 1
+                    nums.append(get_number_from_matrix(matrix, j, i+1, direction="left"))
+                elif bottom_right.isdigit():
+                    nums_found = nums_found + 2
+                    nums.append(get_number_from_matrix(matrix, j-1, i+1, direction="left"))
+                    nums.append(get_number_from_matrix(matrix, j+1, i+1, direction="right"))
+                else:
+                    nums_found = nums_found + 1
+                    nums.append(get_number_from_matrix(matrix, j-1, i+1, direction="left"))
+            elif bottom_right.isdigit():
+                nums_found = nums_found + 1
+                nums.append(get_number_from_matrix(matrix, j+1, i+1, direction="right"))
+            if nums_found > 1:
+                # get gear ratio
+                ratio = nums[0] * nums[1]
+                total = total + ratio
+                print(f"{nums_found} found -> {i}:{j} -> {nums[0]} * {nums[1]} = {ratio} -> cur total = {total}")
+        #     if start_digit == -1:
+        #         start_digit = j
+        #     elif j == len(matrix[i])-1 and end_digit == -1:
+        #         end_digit = j
+        # else:
+        #     if end_digit == -1 and start_digit != -1:
+        #         end_digit = j - 1
+        # if start_digit >= 0 and end_digit >= 0:
+        #     # now that we have a number, check if a symbol exists
+        #     for k in range(max(0, i - 1), min(len(matrix), i + 2)):
+        #         for m in range(
+        #             max(0, start_digit - 1), min(len(matrix[i]), end_digit + 2)
+        #         ):
+        #             part_num_array = []
+        #             if not matrix[k][m].isdigit() and matrix[k][m] != ".":
+        #                 # this one counts, find the number
+        #                 idx = start_digit
+        #                 while idx < end_digit + 1:
+        #                     part_num_array.append(matrix[i][idx])
+        #                     idx = idx + 1
+        #                 part_num = int("".join(part_num_array))
+        #                 print(part_num)
+        #                 total = total + part_num
+        #     start_digit = -1
+        #     end_digit = -1
 
 print(total)
